c32e1c20fa4cc82b96c3e35c39e85937d5deb8e4
Dhilipkumar P <141503885+07dhilipkumar02@users.noreply.github.com>
2026-04-09 16:09:51 +0530

    Update email configuration and clean up code

--- backend/controllers/portfolioController.js
+++ backend/controllers/portfolioController.js
@@ -25,8 +25,8 @@ const sendEmailController = async (req, res) => {
     //email matter
      await Contact.create({ name, email, msg });
        const info =  await transporter.sendMail({
-      to: "vlakshmidhilip@gmail.com",
-      from:"ddhilip613@gmail.com",
+      to: "Enter your mail",
+      from:"Enter your mail",
       replyTo: email,
 
       subject: "Regarding Mern Portfolio App",
@@ -55,61 +55,7 @@ const sendEmailController = async (req, res) => {
       error,
     });
   }
-}; 
+};  
 
-module.exports = { sendEmailController };
-
-// const nodemailer = require("nodemailer");
-// const sendGridTransport = require("nodemailer-sendgrid-transport");
-
-// const transporter = nodemailer.createTransport(
-//   sendGridTransport({
-//     auth: {
-//       api_key: process.env.API_SENDGRID,
-//     },
-//   })
-// );
-
-// const sendEmailController = async (req, res) => {
-//   try {
-//     const { name, email, msg } = req.body;
-
-//     if (!name || !email || !msg) {
-//       return res.status(400).send({
-//         success: false,
-//         message: "Please Provide All Fields",
-//       });
-//     }
-
-//     const info = await transporter.sendMail({
-//       to: "dhilip613@gmail.com",
-//       from: "dhilip613@gmail.com",
-//       replyTo: email,
-//       subject: "Regarding MERN Portfolio App",
-//       html: `
-//         <h5>Detail Information</h5>
-//         <ul>
-//           <li><p><b>Name:</b> ${name}</p></li>
-//           <li><p><b>Email:</b> ${email}</p></li>
-//           <li><p><b>Message:</b> ${msg}</p></li>
-//         </ul>
-//       `,
-//     });
-
-//     console.log("Mail sent info:", info);
-
-//     return res.status(200).send({
-//       success: true,
-//       message: "Your Message Sent Successfully",
-//     });
-//   } catch (error) {
-//     console.log("Send Email Error:", error.response?.body || error.message || error);
-
-//     return res.status(500).send({
-//       success: false,
-//       message: "Send Email API Error",
-//     });
-//   }
-// };
-
-// module.exports = { sendEmailController };
+module.exports = { sendEmailController };          
+         
